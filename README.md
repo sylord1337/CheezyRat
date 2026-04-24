8d7bcd5979954bd1c1b71de6ff95ad19007c4e21
Sylord <vipuser228@outlook.com>
2026-04-25 01:47:06 +0200

    Init client

--- README.md
+++
@@ -1,2 +0,0 @@
-# CheezyRat
-A special tool that can help you get access to another computer using network.
--- server/build/Desktop_Qt_6_11_0_MSVC2022_64bit-u041eu0442u043bu0430u0434u043au0430/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_server_FILE C:/Users/1/Desktop/cheezyRat/server/build/Desktop_Qt_6_11_0_MSVC2022_64bit-u041eu0442u043bu0430u0434u043au0430/server.exe)
-set(__QT_DEPLOY_TARGET_server_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_server_RUNTIME_DLLS D:/Qt/6.11.0/msvc2022_64/bin/Qt6Widgetsd.dll;D:/Qt/6.11.0/msvc2022_64/bin/Qt6Guid.dll;D:/Qt/6.11.0/msvc2022_64/bin/Qt6Cored.dll)
