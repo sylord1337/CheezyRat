fedd666a19a26a907780e2f3a628fc95f7c312f5
Sylord <vipuser228@outlook.com>
2026-04-25 01:39:03 +0200

    Build Fix

--- server/build/Desktop_Qt_6_11_0_MSVC2022_64bit-u041eu0442u043bu0430u0434u043au0430/.qt/deploy_server_4a93cbd249.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/1/Desktop/CheezyRat/server/build/Desktop_Qt_6_11_0_MSVC2022_64bit-u041eu0442u043bu0430u0434u043au0430/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/server-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "C:/Users/1/Desktop/CheezyRat/server/build/Desktop_Qt_6_11_0_MSVC2022_64bit-u041eu0442u043bu0430u0434u043au0430/server.exe"
-    GENERATE_QT_CONF
-)
