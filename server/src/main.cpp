21b074dd12ac2a10cc1e790e5a553c7ee0c67e81
Sylord <vipuser228@outlook.com>
2026-04-29 00:53:17 +0200

    Fix merge

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -5,12 +5,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-<<<<<<<< HEAD:server/src/main.cpp
     Network net;
     net.ServerStartup();
-========
-    ClientStartup();
->>>>>>>> client:client/src/main.cpp
     MainWindow w;
     w.show();
     return QCoreApplication::exec();
